c288d79827d7e4b9a6cdc491ac7f0457edb40265
Christofer Berruz Chungata <cberruzc@my.bridgeport.edu>
2021-02-18 20:36:00 -0500

    Added electron is dev functionality

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,37 @@
 import { app, BrowserWindow } from 'electron';
 declare const MAIN_WINDOW_WEBPACK_ENTRY: any;
 
+import isDev from 'electron-is-dev';
+
 // Handle creating/removing shortcuts on Windows when installing/uninstalling.
 if (require('electron-squirrel-startup')) { // eslint-disable-line global-require
   app.quit();
 }
 
 const createWindow = (): void => {
-  // Create the browser window.
-  const mainWindow = new BrowserWindow({
-    height: 800,
-    width: 480,
+
+  let options:Electron.BrowserWindowConstructorOptions = {
     webPreferences:{
-      nodeIntegration: true,
-    },
-  });
+      nodeIntegration: true
+    }
+  };
+
+  if (!isDev){
+    options = {...options, ...{fullscreen: true, simpleFullscreen: true}};
+    options.webPreferences = {...options.webPreferences, ...{devTools: false}};
+  }
+
+  // Create the browser window.
+  const mainWindow = new BrowserWindow(options);
 
   // and load the index.html of the app.
   mainWindow.loadURL(MAIN_WINDOW_WEBPACK_ENTRY);
 
   // Open the DevTools.
-  mainWindow.webContents.openDevTools();
+  if(isDev){
+    mainWindow.webContents.openDevTools();
+  }
+
 };
 
 // This method will be called when Electron has finished
