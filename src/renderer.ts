fbaaa60c2f9394a1c29562aa548008f97c8d60f4
Christofer Berruz Chungata <cberruzc@my.bridgeport.edu>
2021-04-22 11:40:19 -0400

    Works

--- src/renderer.ts
+++ src/renderer.ts
@@ -1,27 +1,55 @@
 import './index.css';
 import './App';
 
-import {photoCamera, videoCamera, sensor} from "./cleanhands-utils/cleanhands";
-import {io} from "socket.io-client";
+import {videoCamera, sensor} from "./cleanhands-utils/cleanhands";
+import {io, Socket} from "socket.io-client";
 
 // Websocket information
-const serverName = 'https://02daddbf936f.ngrok.io';
+const serverName = 'https://cleanhands-flask-server-p644b.ondigitalocean.app';
 const framesNamespace = 'pi-frames';
 
+const handwashingTimeReport = document.getElementById('handwashingTimeReport');
 
-function HandleConnectVideoSocket(videoSocket:any)
+
+function WriteToScreen(message:string)
+{
+    handwashingTimeReport.innerHTML = message;
+    // Set a timeout of 3 seconds to clear screen message
+    setTimeout(() => {
+        handwashingTimeReport.innerHTML = "";
+    },3000);
+}
+function HandleConnectVideoSocket(videoSocket:Socket)
 {
     // Attach callback on videostream
     videoCamera.on('frame', (data) => {
         videoSocket.emit('frame', data);
     });
 
-    // Record for 10 second and disconnect from socket
-    videoCamera.startCapture().then(() => {
-        setTimeout(() => {
-            videoCamera.stopCapture();
-            videoSocket.disconnect();
-        }, 1000);
+    let cleanUpResources = () => {
+        videoCamera.stopCapture();
+        videoSocket.emit('reset');
+        videoSocket.disconnect();
+    }
+
+    // We clear up the socket connection after we get a result
+    videoSocket.on('result', (data:number) => {
+        console.log(`Received the following result: ${data}`);
+        cleanUpResources();
+        let resultMessage = " Your handwashing time is: " + data.toString() + " seconds";
+        WriteToScreen(resultMessage);
+    });
+
+    // Record indefinitely until we get result from server
+    videoCamera.startCapture();
+
+    sensor.on('leave', () => {
+        if(videoSocket.connected)
+        {
+            cleanUpResources();
+            let resultMessage = "";
+            WriteToScreen(resultMessage);
+        }
     });
 }
 
@@ -29,7 +57,7 @@ function HandleConnectVideoSocket(videoSocket:any)
 function HandleCloseness()
 {
     // Create a connection to the video socket
-    const videoSocket = io(`${serverName}/${framesNamespace}`);
+    const videoSocket:Socket = io(`${serverName}/${framesNamespace}`);
     console.log(`${serverName}/${framesNamespace}`);
 
     videoSocket.on("connect", () => {
@@ -40,7 +68,12 @@ function HandleCloseness()
     videoSocket.on("connect_error", (e:any) => console.log(`Something went wrong: ${e}`));
 }
 
-// Fake timeout to test sockets
-setTimeout(() => {
+const sensorNearHandler = (distance:number) => {
+    console.log(`Someone is near at ${distance} cm.`);
     HandleCloseness();
-}, 15000);
+}
+
+sensor.on('near', sensorNearHandler);
+
+sensor.startSensing();
+//setTimeout(HandleCloseness, 15000);
